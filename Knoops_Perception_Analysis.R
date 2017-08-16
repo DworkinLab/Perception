9d1efbe6d6feaf6fa135d90e77873f644fa20472
Paul Knoops <knoopsp@mcmaster.ca>
2017-08-16 10:52:22 -0400

    whole bunch

--- Knoops_Perception_Analysis.R
+++ Knoops_Perception_Analysis.R
@@ -14,6 +14,45 @@ library("pbkrtest")
 #Possibly not needed
 library(Hmisc)
 
+ggCaterpillar <- function(re, QQ=TRUE, likeDotplot=TRUE) {
+  require(ggplot2)
+  f <- function(x) {
+    pv   <- attr(x, "postVar")
+    cols <- 1:(dim(pv)[1])
+    se   <- unlist(lapply(cols, function(i) sqrt(pv[i, i, ])))
+    ord  <- unlist(lapply(x, order)) + rep((0:(ncol(x) - 1)) * nrow(x), each=nrow(x))
+    pDf  <- data.frame(y=unlist(x)[ord],
+                       ci=1.96*se[ord],
+                       nQQ=rep(qnorm(ppoints(nrow(x))), ncol(x)),
+                       ID=factor(rep(rownames(x), ncol(x))[ord], levels=rownames(x)[ord]),
+                       ind=gl(ncol(x), nrow(x), labels=names(x)))
+    
+    if(QQ) {  ## normal QQ-plot
+      p <- ggplot(pDf, aes(nQQ, y))
+      p <- p + facet_wrap(~ ind, scales="free")
+      p <- p + xlab("Standard normal quantiles") + ylab("Random effect quantiles")
+    } else {  ## caterpillar dotplot
+      p <- ggplot(pDf, aes(ID, y)) + coord_flip()
+      if(likeDotplot) {  ## imitate dotplot() -> same scales for random effects
+        p <- p + facet_wrap(~ ind)
+      } else {           ## different scales for random effects
+        p <- p + facet_grid(ind ~ ., scales="free_y")
+      }
+      p <- p + xlab("Levels") + ylab("Random effects")
+    }
+    
+    p <- p + theme(legend.position="none")
+    p <- p + geom_hline(yintercept=0)
+    p <- p + geom_errorbar(aes(ymin=y-ci, ymax=y+ci), width=0, colour="black")
+    p <- p + geom_point(aes(size=1.2), colour="blue") 
+    return(p)
+  }
+  
+  lapply(re, f)
+}
+
+
+
 
 perception <- read.csv('Perception_all_Data.csv', h=TRUE)
 
@@ -73,13 +112,16 @@ legend("bottomleft", bty="y",
 mod1 <- glmer(cbind(Spider, Not_spider) ~ 1 + Sex  + (1|Date) 
               + (0 + Sex|DGRP), data = DGRP_by_counts, family = "binomial")
 summary(mod1)
+
 rr1 <- ranef(mod1, condVar = TRUE)
-dotplot(rr1)
+head(rr1)
 pv <- attr(rr1$DGRP, "postVar")
+pv
 se_fem <- pv[1, 1, ]
 se_ma <- pv[2,2, ]
 #Data frame of intercepts
 rand.interc<-rr1$DGRP
+rand.interc
 df<-data.frame(Intercepts=rr1$DGRP[,1:2], DGRP=rownames(rand.interc), se_female=se_fem, se_male=se_ma)
 colnames(df) <- c("Female_Int", "Male_Int", "DGRP", "Female_se", "Male_se")
 df$m.low <- with(df, Male_Int - 2 * Male_se)
@@ -87,17 +129,58 @@ df$m.high <- with(df, Male_Int + 2 * Male_se)
 df$f.low <- with(df, Female_Int - 2 * Female_se)
 df$f.high <- with(df, Female_Int + 2 * Female_se)
 
-ggplot(df, aes(y=Female_Int, x=reorder(DGRP, Female_Int))) + 
+df$Male_scale  <- df$Male_Int + 0.5
+df$Female_scale  <- df$Female_Int + 0.5
+df$m.scale.low  <- df$m.low + 0.5
+df$m.scale.high  <- df$m.high + 0.5
+df$f.scale.low  <- df$f.low + 0.5
+df$f.scale.high  <- df$f.high + 0.5
+
+
+
+summary(mod1)
+cc1 <- coef(mod1)$DGRP
+cc1$DGRP <- df$DGRP
+head(cc1)
+
+F1 <- ggplot(df, aes(y=Female_Int, x=reorder(DGRP, Female_Int))) + 
   geom_linerange(aes(ymin=f.low, ymax=f.high), colour="black") + 
+  geom_point(colour="red", alpha=.5) + 
+  coord_flip() + 
+  labs(y="Intercept", x="DGRP Females")
+# Error bars == upper and lower 95% confidence intervals
+M1 <- ggplot(df, aes(y=Male_Int, x=reorder(DGRP, Male_Int))) + 
+  geom_linerange(aes(ymin=m.low, ymax=m.high), colour="black") + 
+  geom_point(colour="blue", alpha=.5) + 
+  coord_flip() + 
+  labs(y="Intercept", x="DGRP Males")
+
+
+F1
+M1
+
+VarCorr(mod1)
+line3 <- lm(df$Female_Int ~ df$Male_Int)
+
+with(df, plot(x = Female_Int,y = Male_Int, xlab = "Females", ylab = "Males", abline(line3), main = "Male Female correlation: proportion with spider"))
+
+
+F2 <- ggplot(df, aes(y=Female_scale, 
+                     x=reorder(DGRP, Female_scale))) + 
+  geom_linerange(aes(ymin=f.scale.low, ymax=f.scale.high), colour="black") + 
   geom_point(colour="red") + 
   coord_flip() + 
   labs(y="Intercept", x="DGRP Females")
-
-ggplot(df, aes(y=Male_Int, x=reorder(DGRP, Male_Int))) + 
-  geom_linerange(aes(ymin=m.low, ymax=m.high), colour="black") + 
-  geom_point(colour="blue") + 
+#F2
+# Error bars == upper and lower 95% confidence intervals
+M2 <- ggplot(df, aes(y=Male_scale, x=reorder(DGRP, Male_scale))) + 
+  geom_linerange(aes(ymin=m.scale.low, ymax=m.scale.high), colour="black") + 
+  geom_point(colour="blue", alpha=.5) + 
   coord_flip() + 
   labs(y="Intercept", x="DGRP Males")
+#M2
+
+
 
 #Model comparison: comparing with random DGRP effects and without:
 
@@ -127,3 +210,4 @@ summary(PB_Mod_1)
 PB_Mod_2 <- PBmodcomp(mod1, mod2, nsim = 10, ref = NULL, seed = NULL,
                     cl = NULL, details = 0)
 summary(PB_Mod_2)
+
